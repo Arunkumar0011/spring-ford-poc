1c3824f0cc6f8ae91c52de97b7dbd055be025298
Arunkumar0011 <arunbinarycode@gmail.com>
2023-12-11 18:38:31 +0530

    First Commit

---
+++ src/main/java/com/ford/azure/poc/SpringFordPocApplication.java
@@ -0,0 +1,13 @@
+package com.ford.azure.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFordPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFordPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ford/azure/poc/SpringFordPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.azure.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFordPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
